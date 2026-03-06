fd4177926664176b8b8c1b7a98a4df8309443d13
Praveen007-m <praveensk3737@gmail.com>
2026-03-06 11:58:56 +0530

    Allow all CORS for deployment

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,26 +1,26 @@
 export const API_BASE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_SERVER + "api/"
+    ? (import.meta.env.VITE_BACKEND_SERVER || "https://idurar-erp-crm-gyyk.onrender.com/") + "api/"
     : "http://localhost:8888/api/";
 
 export const BASE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_SERVER
+    ? (import.meta.env.VITE_BACKEND_SERVER || "https://idurar-erp-crm-gyyk.onrender.com/")
     : "http://localhost:8888/";
 
 export const WEBSITE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_SERVER
+    ? (import.meta.env.VITE_BACKEND_SERVER || "https://idurar-erp-crm-gyyk.onrender.com/")
     : "http://localhost:3000/";
 
 export const DOWNLOAD_BASE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_SERVER + "download/"
+    ? (import.meta.env.VITE_BACKEND_SERVER || "https://idurar-erp-crm-gyyk.onrender.com/") + "download/"
     : "http://localhost:8888/download/";
 
 export const ACCESS_TOKEN_NAME = "x-auth-token";
 
 export const FILE_BASE_URL =
   import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_SERVER
+    ? (import.meta.env.VITE_BACKEND_SERVER || "https://idurar-erp-crm-gyyk.onrender.com/")
     : "http://localhost:8888/";
